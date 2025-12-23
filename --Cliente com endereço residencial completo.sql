e22f6f2f826700329abfe03fb30e2ad980b97644
LorenaPassagem <lorenapassagem@gmail.com>
2025-12-23 08:54:05 -0300

    Delete --Cliente com endereço residencial completo.sql

--- --Cliente com endereço residencial completo.sql
+++
@@ -1,9 +0,0 @@
---Cliente com endereço residencial completo
-select p.ID_PESSOA, p.NOME, e.NUMERO, c.CIDADE, es.ESTADO, te.TIPO_ENDERECO
-from m1.PESSOAS p 
-right join m1.ENDERECOS e on e.ID_PESSOA = p.ID_PESSOA
-inner join m1.CIDADES c on c.ID_CIDADE = e.ID_CIDADE
-inner join m1.ESTADO ES on ES.ID_ESTADO = c.ID_ESTADO
-inner join m1.TIPO_ENDERECO TE on e.ID_TIPO_ENDERECO = TE.ID_TIPO_ENDERECO
-where e.ID_TIPO_ENDERECO=1
-order by 5
